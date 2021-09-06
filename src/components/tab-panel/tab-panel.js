cefcfd768bc02c209e173b3f9eae4912df02f261
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-06 19:17:40 +0300

    Add files via upload

---
+++ src/components/tab-panel/tab-panel.js
@@ -0,0 +1,27 @@
+import React        from 'react';
+import { Link }     from 'react-router-dom'
+
+import './tab-panel.css';
+
+const TabPanel = ({ data, name }) => {
+
+  const items = data.map((item,idx) => {
+    const active = name === item ? 'active' : ''
+    return (
+        <li key={idx} className="nav-item">
+            <Link   className   ={ `nav-link ${ active }` }
+                    to          ={ `/${item}` }
+                >{item}
+            </Link>
+        </li>
+      )
+  })
+  return (
+      <div className="header-my">
+          <ul className="nav nav-tabs">
+              {items}
+          </ul>
+      </div>
+    )
+  }  
+export default TabPanel;
