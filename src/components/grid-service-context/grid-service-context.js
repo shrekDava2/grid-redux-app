ee66da45ff7bb9a527ba5414d6191c2ec1204aef
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-06 19:18:55 +0300

    Add files via upload

---
+++ src/components/grid-service-context/grid-service-context.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const {
+  Provider: GridServiceProvider,
+  Consumer: GridServiceConsumer
+} = React.createContext();
+
+export {
+  GridServiceProvider,
+  GridServiceConsumer
+};
