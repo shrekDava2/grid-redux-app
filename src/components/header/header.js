5e028230ffcf09c1be622b213796156d12d1ff30
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-06 19:18:40 +0300

    Add files via upload

---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header d-flex">
+      <h3> 
+          Welcome to Grid App
+      </h3>
+    </div>
+  );
+};
+
+export default Header;
