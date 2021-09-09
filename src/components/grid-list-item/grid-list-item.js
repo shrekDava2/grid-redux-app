e81ef5bf9942daaeb186807f43c233bcafe5d96b
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-09 19:40:00 +0300

    Add files via upload

--- src/components/grid-list-item/grid-list-item.js
+++ src/components/grid-list-item/grid-list-item.js
@@ -1,29 +1,47 @@
-import React ,{Component} from 'react'
+import React ,{Component}     from 'react'
+import Buttom                 from '../button'
 
 import './grid-list-item.css'
 
 export default class GridListItem extends Component {
 
   render(){
-    const { id, title, leftButton,rightButton, checked, toggleCheckbox } = this.props  
+    const { id, title, ids, addClass, checked, toggleCheckbox, checkedAllInd, onMoveLeft, onMoveRight } = this.props  
     return (
       <li key={ id } 
-          className="list-group-item">
-        <input  type="checkbox" 
-                onChange = { toggleCheckbox }
-                value={ id }
-                checked = { checked } />
+          className={`list-group-item ${ addClass }`}>
+        <input  
+            type      ="checkbox" 
+            ref       = { el => el && (el.indeterminate = checkedAllInd) }
+            onChange  = { toggleCheckbox }
+            value     = { id }
+            checked   = { checked } 
+        />
         <span >
           <span className="todo-list-item-label">
             { title }
-          </span>    
-          { rightButton }
-          { leftButton }
-
+          </span>   
+          { onMoveRight === defFunc ? undefined : 
+                <Buttom 
+                    side    = { 'right' }
+                    onMove  = { () => onMoveRight(ids) }
+                />             
+          }
+          { onMoveLeft === defFunc ? undefined : 
+                <Buttom 
+                    side    = { 'left' }
+                    onMove  = { () => onMoveLeft(ids) }  
+                /> 
+          }         
         </span>
       </li>
     )
   }
+}
+const defFunc = () => { return false }
+GridListItem.defaultProps = {
+  onMoveLeft: defFunc,
+  onMoveRight: defFunc
 }
 
 
