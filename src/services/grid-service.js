f4689864999ed7821da1b88206fec8f28e33175c
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-09 19:36:10 +0300

    Add files via upload

--- src/services/grid-service.js
+++ src/services/grid-service.js
@@ -1,31 +1,7 @@
 
 export default class GridService {
-
-    data ={ 
-        left: [
-            {id: 0, title: 'Left 0'},
-            {id: 1, title: 'Left 1'},
-            {id: 2, title: 'Left 2'},
-        ],
-        middle: [
-            {id: 3, title: 'Middle 0'},
-            {id: 4, title: 'Middle 1'},
-            {id: 5, title: 'Middle 2'},
-        ],
-        right: [
-            {id: 6, title: 'Right 0'},
-            {id: 66, title: 'Right 1'},
-            {id: 666, title: 'Right 2'},
-        ]
-      
-    }; 
-    
-    getGrids() {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            resolve(this.data)
-          }, 700);
-        });
-      }
-  
+  getGrids = async () => {
+    const res = await fetch('http://localhost:3000/data');
+    return await res.json();
+  } 
 }
