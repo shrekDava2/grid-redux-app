1de91cb82002422699a786a47fb9705f8fb02d22
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-09 19:39:06 +0300

    Add files via upload

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,16 +1,21 @@
 
 import React, { Component }   from 'react'
-import './button.css'
 
-export default class Button extends Component {
+ class Button extends Component {
     render(){
-        const { side , onMove } = this.props  
-    
-        return(    <button type="button "  
-                            className={`btn  btn-sm float-right ${side}-button`}
-                            onClick ={ onMove }  >
-                        <i  className={`fa fa-arrow-${side}`} />
+        const { side, onMove } = this.props  
+        return(    <button 
+                            type        = "button"  
+                            className   = { `btn  btn-sm float-right ${side}-button` }
+                            onClick     = { onMove }  >
+                        <i  
+                            className   = { `fa fa-arrow-${side}` } />
                     </button>
             )   
         }
 }
+
+Button.defaultProps = {
+    onMove: () => {}
+  }
+export default Button
