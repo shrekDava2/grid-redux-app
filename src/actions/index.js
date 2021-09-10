b57b7c27baedda9ba6507a8230a26364f2c5343e
shrekDava2 <89479400+shrekDava2@users.noreply.github.com>
2021-09-10 16:39:33 +0300

    Add files via upload

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,21 +5,21 @@ const gridLoaded = newItems => {
   return {
     type: actionsType.FETH_GRID_SUCCESS,
     payload: newItems
-  };
-};
+  }
+}
 
 const gridRequested = () => {
   return {
     type: actionsType.FETH_GRID_REQUEST
-  };
-};
+  }
+}
 
 const gridError = error => {
   return {
     type: actionsType.FETH_GRID_FAILURE,
     payload: error
-  };
-};
+  }
+}
 
 const onMove = prop => {
   return{
@@ -27,25 +27,18 @@ const onMove = prop => {
     payload: prop
   }
 }
-/*
-const fetchKeys = () => {
-  return{
-    type: actionsType.FETH_KEYS
-  }
-}
-*/
+
 const fetchGrids = (gridService, dispatch) => () => {
   dispatch(gridRequested())
   gridService.getGrids()
-            .then( data   => dispatch(gridLoaded(data)))
-            .catch( error => dispatch(gridError(error)))
+            .then(data => dispatch(gridLoaded(data)))
+            .catch(error => dispatch(gridError(error)))
 }
 
 export {
   onMove,
   fetchGrids,
-  //fetchKeys,
   gridError,
   gridRequested,
   gridLoaded
-};
+}
